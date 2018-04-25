932e60192874086fc6cfa50cd40c4c804f54bdbb
webbanie <36488667+webbanie@users.noreply.github.com>
2018-04-25 17:41:04 -0400

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -148,8 +148,20 @@
   });
 
 
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var title = button.data('title') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var recipient = button.data('img')
+  var modal = $(this)
+  modal.find('.modal-title').text(title)
+  modal.find('.modal-body img').attr("src", recipient)
+});
 
-})
+
+
+}); //close document ready
 
 
 })(jQuery); // End of use strict
